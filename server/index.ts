75c1b8ca798dee5b4e54eac668d036cfdf21f9ec
Colton Robbins <coltondr@gmail.com>
2023-05-22 21:09:09 -0500

    router & serverless on index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import weatherController from './src/apiController.js';
 import logicController from './src/logicController.js';
 import seasonalFoods from './src/seasonalFoods.js';
 const app = express();
+const router = express.Router();
 const PORT = 8080;
 
 app.use(express.json());
@@ -16,7 +17,7 @@ app.get('/', (req,res) => {
   res.sendFile(`${process.cwd()}../client/dist/index.html`);
 });
 
-app.use('/cityForecast', weatherController.apiData, logicController.forecast, seasonalFoods.foods, (req, res) => {
+router.post('/cityForecast', weatherController.apiData, logicController.forecast, seasonalFoods.foods, (req, res) => {
   console.log('res.locals: ', res.locals)
     res.json(res.locals);
   });
@@ -24,16 +25,17 @@ app.use('/cityForecast', weatherController.apiData, logicController.forecast, se
 /**
  * 404 handler
  */
-app.use('*', (req, res) => {
+router.use('*', (req, res) => {
   res.status(404).send(`${req} 404 Not Found`);
 });
 
 // Global error handler
-app.use((err: any, req: any, res: any, next: any) => {
+router.use((err: any, req: any, res: any, next: any) => {
   console.log(err);
   res.status(500).send('Internal Server Error');
 });
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}...`); });
 
-// module.exports.handler = serverless(app);
+app.use('/.netlify/functions/index', router);
+module.exports.handler = serverless(app);
