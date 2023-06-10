88660f812c78c250b127c23d692730b9c5b4c534
Colton Robbins <coltondr@gmail.com>
2023-06-10 14:40:33 -0500

    changed vite config file to include jsxInject

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  }
 });
