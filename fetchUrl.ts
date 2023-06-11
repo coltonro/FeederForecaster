3261f334973fe2ade292170cccafdc2a456fb87a
Colton Robbins <coltondr@gmail.com>
2023-06-11 13:08:31 -0500

    updated prod fetch url

--- fetchUrl.ts
+++ fetchUrl.ts
@@ -2,6 +2,6 @@
 const dev: string = 'http://localhost:9000/.netlify/functions/index';
 
 // production server
-const prod: string = 'https://vermillion-manatee-983a35.netlify.app/.netlify/functions/api';
+const prod: string = 'https://vermillion-manatee-983a35.netlify.app/.netlify/functions/index';
 
 export const fetchUrl = prod;
