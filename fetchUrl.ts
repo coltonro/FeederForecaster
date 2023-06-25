82e702b501a3091c97622812d9a21161620735ea
Colton Robbins <coltondr@gmail.com>
2023-06-24 23:01:25 -0500

    made forecast bar text on mobile obviously clickable

--- fetchUrl.ts
+++ fetchUrl.ts
@@ -4,4 +4,4 @@ const dev: string = 'http://localhost:9000/.netlify/functions/index';
 // production server
 const prod: string = 'https://vermillion-manatee-983a35.netlify.app/.netlify/functions/index';
 
-export const fetchUrl = prod;
+export const fetchUrl = dev;
